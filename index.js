6cb4aedc23e4bc290428784e1f34519307409f8b
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-02-16 08:42:20 -0300

    validations

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const d = document;
 const billInput = d.getElementById("bill");
+const tipOptionsContainer = d.querySelector(".tip-options");
 const tipOptions = d.querySelectorAll(".tip-options__button");
 const customOptionsInput = d.querySelector(".tip-options__custom");
 const peopleInput = d.getElementById("people-input");
 const personTipElement = d.getElementById("person-tip");
 const personTotalElement = d.getElementById("person-total");
 const resetBtn = d.querySelector(".summary__reset-button");
+const peopleErrorElement = d.querySelector(".error-message__people");
+const billErrorElement = d.querySelector(".error-message__bill");
 
 let bill, percentageOption, people;
 
 billInput.addEventListener("input", function () {
+  if (billInput.value.length > 7) {
+    billErrorElement.innerText = "Max 7 digits";
+    setTimeout(() => {
+      billErrorElement.innerText = "";
+    }, 3000);
+    return;
+  }
   bill = Number(billInput.value);
   calcTip();
 });
 
-d.addEventListener("click", function (e) {
+tipOptionsContainer.addEventListener("click", function (e) {
   if (e.target.matches(".tip-options__button")) {
     tipOptions.forEach((element) => {
       element.innerText === e.target.innerText
@@ -40,6 +50,13 @@ peopleInput.addEventListener("input", function () {
 });
 
 function calcTip() {
+  if (people < 1) {
+    peopleErrorElement.innerText = "Can't be zero";
+    setTimeout(() => {
+      peopleErrorElement.innerText = "";
+    }, 3000);
+  }
+
   if (bill && percentageOption && people) {
     const personTip = Number(((bill * percentageOption) / people).toFixed(2));
     const personTotal = Number((bill / people + personTip).toFixed(2));
